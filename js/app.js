b6ed882935a4c8b89cf781e5db094e90672618b0
Eric K Wu <ekuan.wu@gmail.com>
2014-11-21 18:07:07 -0800

    google map theming

--- js/app.js
+++ js/app.js
@@ -8,3 +8,135 @@ $(document).ready(function(){
 		arrows: false
 	});
 });
+
+(function($) {
+
+function initializeGmap() {
+	var mapOptions = {
+		center: { lat: 34.0522, lng: -118.2437 },
+		zoom: 9,
+		scrollwheel: false,
+		styles: [
+		    {
+		        "featureType": "landscape",
+		        "stylers": [
+		            {
+		                "saturation": -100
+		            },
+		            {
+		                "lightness": 65
+		            },
+		            {
+		                "visibility": "on"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "poi",
+		        "stylers": [
+		            {
+		                "saturation": -100
+		            },
+		            {
+		                "lightness": 51
+		            },
+		            {
+		                "visibility": "simplified"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.highway",
+		        "stylers": [
+		            {
+		                "saturation": -100
+		            },
+		            {
+		                "visibility": "simplified"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.arterial",
+		        "stylers": [
+		            {
+		                "saturation": -100
+		            },
+		            {
+		                "lightness": 30
+		            },
+		            {
+		                "visibility": "on"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "road.local",
+		        "stylers": [
+		            {
+		                "saturation": -100
+		            },
+		            {
+		                "lightness": 40
+		            },
+		            {
+		                "visibility": "on"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "transit",
+		        "stylers": [
+		            {
+		                "saturation": -100
+		            },
+		            {
+		                "visibility": "simplified"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "administrative.province",
+		        "stylers": [
+		            {
+		                "visibility": "off"
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "water",
+		        "elementType": "labels",
+		        "stylers": [
+		            {
+		                "visibility": "on"
+		            },
+		            {
+		                "lightness": -25
+		            },
+		            {
+		                "saturation": -100
+		            }
+		        ]
+		    },
+		    {
+		        "featureType": "water",
+		        "elementType": "geometry",
+		        "stylers": [
+		            {
+		                "hue": "#ffff00"
+		            },
+		            {
+		                "lightness": -25
+		            },
+		            {
+		                "saturation": -97
+		            }
+		        ]
+		    }
+		]
+	};
+	var map = new google.maps.Map(document.getElementById('map-container'), mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initializeGmap);
+
+})(jQuery);
